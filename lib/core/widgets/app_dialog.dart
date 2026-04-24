f143448bf90b1e3bf33fc173f644c9323a82e70d
BadrSayed2 <BadrSayed2@example.com>
2026-04-25 00:34:10 +0300

    Update generated plugin registrant files for consistency

--- lib/core/widgets/app_dialog.dart
+++ lib/core/widgets/app_dialog.dart
@@ -5,26 +5,30 @@ abstract class AppDialog {
   static void showLoadingUi(BuildContext context) {
     showDialog(
       context: context,
-      builder: (context) => AlertDialog(
-        backgroundColor: ColorsApp.backgroundColor,
-        content: SizedBox(
-          height: 40,
-          width: 40,
-          child: Center(
-            child: Row(
-              spacing: 30,
-              mainAxisAlignment: .center,
-              children: [
-                CircularProgressIndicator(color: ColorsApp.primaryColor),
-                Text(
-                  "Loading...",
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: .w400,
-                    color: ColorsApp.textColor,
+      barrierDismissible: false,
+      builder: (context) => PopScope(
+        canPop: false,
+        child: AlertDialog(
+          backgroundColor: ColorsApp.backgroundColor,
+          content: SizedBox(
+            height: 40,
+            width: 40,
+            child: Center(
+              child: Row(
+                spacing: 30,
+                mainAxisAlignment: .center,
+                children: [
+                  CircularProgressIndicator(color: ColorsApp.primaryColor),
+                  Text(
+                    "Loading...",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: .w400,
+                      color: ColorsApp.textColor,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
