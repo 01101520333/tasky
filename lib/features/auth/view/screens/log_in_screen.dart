f59df7295d39030aabf5b3ae557b72978557f4bf
BadrSayed2 <BadrSayed2@example.com>
2026-04-28 00:41:54 +0300

    Refactor home screen structure; move home_screen.dart to the correct directory and implement logout functionality with UI updates.

--- lib/features/auth/view/screens/log_in_screen.dart
+++ lib/features/auth/view/screens/log_in_screen.dart
@@ -7,7 +7,7 @@ import 'package:tasky/features/auth/view/widgets/matreial_button_widget.dart';
 import 'package:tasky/features/auth/view/widgets/state_user_auth.dart';
 import 'package:tasky/features/auth/view/widgets/text_form_field_widget.dart';
 import 'package:tasky/features/auth/view_model/auth_cubit.dart';
-import 'package:tasky/features/home/screens/home_screen.dart';
+import 'package:tasky/features/home/view/screens/home_screen.dart';
 
 class LogInScreen extends StatefulWidget {
   LogInScreen({super.key});
---
+++ lib/features/home/view/screens/home_screen.dart
@@ -0,0 +1,93 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:tasky/core/utils/assets_icons.dart';
+import 'package:tasky/core/utils/assets_images.dart';
+import 'package:tasky/core/utils/colors_app.dart';
+import 'package:tasky/features/auth/view/screens/log_in_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+  static const String routeName = "HomeScreen";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Image.asset(AssetsIcons.logoIcon, width: 90),
+
+            Spacer(),
+
+            Image.asset(AssetsIcons.logoutIcon, height: 30, width: 30),
+            SizedBox(width: 5),
+            GestureDetector(
+              onTap: () async {
+                await FirebaseAuth.instance.signOut();
+                if (!context.mounted) return;
+
+                Navigator.of(
+                  context,
+                ).pushReplacementNamed(LogInScreen.routeName);
+              },
+              child: Text(
+                "Log out",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: .w400,
+                  color: Color(0xffFF4949),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+
+      body: EmptyHomeScreen(),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Color(0xff24252C),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadiusGeometry.circular(30),
+        ),
+        child: Icon(Icons.add, size: 30, color: Color(0xff5F33E1)),
+      ),
+    );
+  }
+}
+
+class EmptyHomeScreen extends StatelessWidget {
+  const EmptyHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+      child: Column(
+        children: [
+          SizedBox(height: 100),
+          Image.asset(AssetsImages.emptyScreenImage),
+          SizedBox(height: 5),
+          Text(
+            "What do you want to do today?",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: .w400,
+              color: ColorsApp.textColor,
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            "Tap + to add your tasks",
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: .w400,
+              color: Color(0xff404147),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
